a02d7d4d72f31cf58c5722fb8b29c814441ada93
Holger Frydrych <frydrych@mailbox.org>
2021-12-29 20:25:21 +0100

    Hook Oculus GetHmdDesc

--- src/hooks/oculus_hooks.cpp
+++ src/hooks/oculus_hooks.cpp
@@ -2,8 +2,14 @@
 #include "hooks.h"
 #include "logging.h"
 #include "win_header_sane.h"
+#include "OVR_CAPI.h"
 
 namespace {
+	ovrHmdDesc ovrHook_GetHmdDesc(ovrSession session) {
+		ovrHmdDesc result = vrperfkit::hooks::CallOriginal(ovrHook_GetHmdDesc)(session);
+		LOG_DEBUG << "ovr_GetHmdDesc called. Headset resolution: " << result.Resolution.w << "x" << result.Resolution.h;
+		return result;
+	}
 }
 
 namespace vrperfkit {
@@ -15,14 +21,18 @@ namespace vrperfkit {
 				return;
 			}
 
-			std::wstring dllName = L"OVRPlugin.dll";
+#ifdef WIN64
+			std::wstring dllName = L"LibOVRRT64_1.dll";
+#else
+			std::wstring dllName = L"LibOVRRT32_1.dll";
+#endif
 			HMODULE handle;
 			if (!GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_PIN, dllName.c_str(), &handle)) {
 				return;
 			}
 
 			LOG_INFO << dllName << " is loaded in the process, installing hooks...";
-			// TODO...
+			InstallHookInDll("ovr_GetHmdDesc", handle, ovrHook_GetHmdDesc);
 
 			hooksLoaded = true;
 		}
